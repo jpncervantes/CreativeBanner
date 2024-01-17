0671ca10cb56546ab5e7601ded83626347a1123f
John Paul N. Cervantes <johnpaulncervantes@gmail.com>
2024-01-17 22:51:30 +0800

    added frame 2

---
+++ ad banner/index.js
@@ -0,0 +1,18 @@
+//headphone movement
+
+const headphone = document.getElementById("headphone");
+
+headphone.addEventListener(
+    "mousemove",
+    (calculateMousePosition = (e) => {
+        var x = e.clientX;
+        var y = e.clientY;
+        headphone.style.left = x + px;
+    })
+);
+
+const firstFrame = document.getElementById("first");
+
+setTimeout(() => {
+    firstFrame.style.display = "none";
+}, 6000);
